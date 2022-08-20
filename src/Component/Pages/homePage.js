3a2b92c9c06ad1c24df4b9e80bdbe3b1c48ffb02
lsj3284 <lsj883283@gmail.com>
2022-08-21 00:09:40 +0900

    충돌2

--- src/Component/Pages/homePage.js
+++ src/Component/Pages/homePage.js
@@ -68,10 +68,6 @@ const Home = () => {
                 </Box> */}
 
             </LeftWrap>
-
-
-
-
             <CenterWrap>
                 <FeedWrap>
                     <Div onClick={onProfile}><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
